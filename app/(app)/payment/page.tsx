e3f124cf9900c0b07cac4a6466ed9d095c0da645
subash.sakthivel <subashsakthi01@gmail.com>
2025-07-12 01:15:57 +0530

    checkout

---
+++ app/(app)/payment/page.tsx
@@ -0,0 +1,12 @@
+import Razorpay from "razorpay";
+import React from "react";
+
+const PaymentPage = ({ params }: { pa }) => {
+  return (
+    <div>
+      <Razorpay />
+    </div>
+  );
+};
+
+export default PaymentPage;
--- app/api/createOrder/route.ts
+++ app/api/createOrder/route.ts
@@ -1,12 +1,61 @@
+import { authOptions } from "@/lib/authOptions";
+import { PaymentService } from "@/lib/payment";
 import { DataModel } from "@/model/DataModels";
 import { ProductModel } from "@/model/Product";
+import { TOrder } from "@/schema/Order";
 import { TProductVariant } from "@/schema/ProductVarient";
+import { TUserInfo } from "@/schema/UserInfo";
 import { FilterState, getData } from "@/util/dataAPI";
-import { NextRequest } from "next/server";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
   //authenticate
-  const { orders, receipt, currency, notes } = await request.json();
+  const session = await getServerSession(authOptions);
+  if (!session?.user.userId) {
+    return NextResponse.json({ message: "Need to login" }, { status: 403 });
+  }
+  const userInfoResData = await getData<TUserInfo>(DataModel["userinfo"], "GET_DATA", {
+    filter: {
+      rules: [
+        {
+          operator: "eq",
+          field: "userId",
+          value: session.user.userId,
+        },
+      ],
+      logicalOperator: "AND",
+    },
+  });
+  if (userInfoResData.docs.length !== 1) {
+    return NextResponse.json({ message: "Invalid User" }, { status: 403 });
+  }
+  const userInfo = userInfoResData.docs[0];
+  const { orders, receipt, currency, notes, shippingAddressId, shippingMethod } =
+    await request.json();
+
+  const orderData: TOrder = {
+    user: userInfo,
+    status: "created",
+    amount: await getTotalAmount(orders),
+    shippingAddress: shippingAddressId,
+    shippingMethod,
+    shippingCost: 0, // todo need calculation
+    trackingNumber: Math.random() + "_",
+    products: orders,
+    notes,
+    currency: "",
+    orderDate: new Date(),
+    transactionId: "",
+  };
+  const paymentService = PaymentService.getInstance();
+  const order = paymentService.createOrder(
+    {
+      ...orderData,
+      notes: notes ? { note: notes } : undefined,
+    },
+    orderData
+  );
   if (!orders || !Array.isArray(orders) || orders.length === 0) {
     return new Response("Invalid order data", { status: 400 });
   }
@@ -14,7 +63,7 @@ export async function POST(request: NextRequest) {
 
 export async function getTotalAmount(
   orders: { productId: string; variantId?: string; quantity: number }[]
-) {
+): Promise<number> {
   const products = await ProductModel.find({
     _id: { $in: orders.map((order) => order.productId), stockQuantity: { $gte: 1 } },
   }).populate({
@@ -37,7 +86,7 @@ export async function getTotalAmount(
       total +=
         variant !== undefined ? variant.variantSpecificPrice ?? variant.variantSpecificPrice : 0;
     } else {
-      total += product.discountedPrice;
+      total += product.discountPrice;
     }
     return total;
   }, 0);
